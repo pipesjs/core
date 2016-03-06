274f59f3eef91516d809b25935d8ee9c5415c2f2
Diwank Singh <diwank.singh@gmail.com>
2016-03-06 12:48:18 -0500

    Added an implementation of chain
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ src/chain.js
@@ -0,0 +1,39 @@
+// chain :: TransformStreams... -> { readable, writable }
+// chain function takes one or more
+// transform streams / { readable, writable } pairs
+// connects them to each other,
+// takes the readable of the end and the writable of the head,
+// returns the { readable, writable } pair that is
+// compatible with `ReadableStream::pipeThrough`
+//
+
+import connect from "./connect";
+
+const
+  isCompatible = s => s && s.readable && s.writable,
+  isReadable = s => s && s.pipeThrough,
+  compatibilityError = `
+    Only transform streams and readable-writable pairs can be chained
+  `;
+
+export default function chain(origin, ...streams) {
+
+  // Check that origin is a transform stream / { readable, writable }
+  if ( !isCompatible( origin ))
+    throw new Error( compatibilityError );
+
+  // connect the streams
+  const
+    { writable } = origin,
+    readable = connect( origin, ...streams );
+
+  // Check if null stream
+  if ( !isReadable( readable ))
+    throw new Error( compatibilityError );
+
+  // return readable-writable pair
+  return {
+    readable,
+    writable
+  };
+}
