f8358e05502a06e53b971d9892549116a7a71ce3
Diwank Singh <diwank.singh@gmail.com>
2016-03-08 16:28:02 -0500

    Added test for chain
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ test/test_chain.js
@@ -0,0 +1,36 @@
+import assert from "assert";
+import chain from "../src/chain";
+import connect from "../src/connect";
+import pipe from "../src/pipe";
+import {
+  createTestWritable,
+  createTestReadable,
+  broker
+} from "./utils";
+
+suite("chain");
+
+test("check chaining", done => {
+  let readable, writable, transform, testChain;
+
+  // Create test streams
+  readable = createTestReadable( [1,2,3] );
+  transform = pipe( k=>k );
+  writable = createTestWritable( assert );
+
+  // End case
+  broker.on(writable.signals.close, done);
+
+  // Connect the streams
+  assert.doesNotThrow( () => {
+    testChain = chain(
+      new transform,
+      new transform,
+      new transform
+    );
+
+    connect( readable, testChain, writable );
+  });
+});
+
+
