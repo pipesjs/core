377932c353996b6cc4f597591cbd1751c1f4d727
Diwank Singh <diwank.singh@gmail.com>
2016-03-20 18:16:57 -0400

    Added test for accumulate
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ test/test_accumulate.js
@@ -0,0 +1,56 @@
+import assert from "assert";
+import accumulate from "../src/accumulate";
+import connect from "../src/connect";
+import {
+  createTestWritable,
+  createTestReadable,
+  broker
+} from "./utils";
+
+suite("accumulate");
+
+test.skip("check reducer", done => {
+  let readable, writable, accumulated, total;
+
+  // Create test streams
+  readable = createTestReadable( [1,2,3] );
+  writable = createTestWritable( val => {
+    total = val;
+  });
+
+  // End case
+  broker.on(writable.signals.close, () => {
+    assert.equal( total, 6 );
+    done();
+  });
+
+  // Connect the streams
+  assert.doesNotThrow( () => {
+    accumulated = accumulate( (a, b) => a+b );
+    connect( readable, (new accumulated), writable );
+  });
+});
+
+test.skip("check reducer with init value", done => {
+  let readable, writable, accumulated, total;
+
+  // Create test streams
+  readable = createTestReadable( [1,2,3] );
+  writable = createTestWritable( val => {
+    total = val;
+  });
+
+  // End case
+  broker.on(writable.signals.close, () => {
+    assert.equal( total, 10 );
+    done();
+  });
+
+  // Connect the streams
+  assert.doesNotThrow( () => {
+    accumulated = accumulate( (a, b) => a+b, 4 );
+    connect( readable, (new accumulated), writable );
+  });
+});
+
+
