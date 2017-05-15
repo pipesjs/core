f446e9cefed2b3fdb0605506b16d59b77eb3d07d
Diwank Singh <diwank.singh@gmail.com>
2017-05-14 21:30:36 -0400

    Added type annotations for split.js
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/zip.js
+++ src/zip.js
@@ -1,3 +1,5 @@
+// @flow
+
 // zip :: ReadableStream... -> ReadableStream
 // zip function takes one or more streams
 // and returns a readable combining the streams,
@@ -7,25 +9,25 @@
 // stream, by waiting for all streams to have pushed a chunk.
 //
 
+import type { ReadableWritable } from "./streams";
+import { ReadableStream } from "./streams";
+
 import merge from "./merge";
 import pipe from "./pipe";
 
-export default function zip(...streams) {
-  let applier, merged;
+export default function zip(...streams: Array<ReadableStream>): ReadableStream {
+  let applier: ReadableWritable,
+      merged: ReadableStream;
 
   // Merge streams
   merged = merge(...streams);
 
   // Create applier
-  applier = pipe( chunks => {
+  applier = new pipe( chunks => {
     let [fn, ...args] = chunks;
     return fn(...args);
   });
 
   // return zipped stream
-  return merged.pipeThrough( new applier );
+  return merged.pipeThrough( applier );
 }
-
-// Browserify compat
-if ( typeof module !== "undefined" )
-  module.exports = zip;
